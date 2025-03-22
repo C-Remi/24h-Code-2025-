dca117c5edf26d1bc2f34153e4fa7c22be9e6926
zephylac <thibo.brocherieux@gmail.com>
2025-03-22 19:34:51 +0100

    refactor

--- src/api/infos.py
+++ src/api/infos.py
@@ -126,28 +126,12 @@ def readInfosRangefinder(data):
 
 def readInfos(data):
     try:
-        view = DataView(data)
-        pos = 0
-
-        mask = view.get_uint_8(pos)
-        pos += 1
-
-        if (mask & INFOS_POSITION):
-            position_x = view.get_float_32(pos + 0)
-            
-            position_y = view.get_float_32(pos + 4)
-            position_a = view.get_float_32(pos + 8)
-            pos += 3*4
-
-            print(f"posX: {round(position_x * 1000)} mm, posY: {round(position_y * 1000)} mm , posA: {round(position_a * 180 / math.pi) } deg")    
-
-        if (mask & INFOS_LED):
-            led_r = view.get_uint_8(pos + 0)
-            led_g = view.get_uint_8(pos + 1)
-            led_b = view.get_uint_8(pos + 2)
-            pos += 3
-
-            print(f"ledR: {led_r}, ledG: {led_g}, ledB: {led_b}")
+        readInfosLed(data)
+        readInfosPos(data)
+        readInfosMotors(data)
+        readInfosWheels(data)
+        readInfosSpeed(data)
+        readInfosRangefinder(data)
     except:
         print('err')
     
---
+++ src/main.py
@@ -0,0 +1,11 @@
+import asyncio
+from robot import Robot
+
+HOST = "haumbot-c5cfb8.local"
+
+async def main():
+    robot = Robot(0,0,0, HOST)
+    await robot.activate()
+
+if __name__ == "__main__":
+    asyncio.run(main())
