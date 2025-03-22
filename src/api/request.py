1914671af31b970599323a724dca7c179fbc87bb
Alexis Masson <a.masson555@ntymail.com>
2025-03-22 23:40:50 +0100

    refactor request.py

--- src/api/request.py
+++ src/api/request.py
@@ -1,42 +1,21 @@
 import requests
 
-def set_led_color(host, color:str = "#000000"):
-    response = requests.post(f"http://{host}/led/set_color", json={
+HOST = "http://haumbot-c5cfb8.local"
+
+def set_led_color(color:str = "#000000"):
+    requests.post(f"{HOST}/led/set_color", json={
         "ledcolor": color
-    })
-    
-    if response.status_code == 200:
-        print(f"Changed led to {color}")
-        return True
-    
-    return False
+    }).raise_for_status()
 
-def turtle_move_forward(host, distance_mm):
-    response = requests.post(f"http://{host}/turtle/send", json={
+def turtle_move_forward(distance_mm):
+    requests.post(f"{HOST}/turtle/send", json={
         "dist": distance_mm, "type": "dist"
-    })
+    }).raise_for_status()
 
-    if response.status_code == 200:
-        return True
-
-    return False
-
-
-def turtle_rotate(host, angle):
-    response = requests.post(f"http://{host}/turtle/send", json={
+def turtle_rotate(angle):
+    requests.post(f"{HOST}/turtle/send", json={
         "angle": angle, "type": "angle"
-    })
+    }).raise_for_status()
 
-    if response.status_code == 200:
-        return True
-
-    return False
-
-def reset_position(host):
-    response = requests.get(f"http://{host}/position/reset")
-    
-    if response.status_code == 200:
-        print('Reset successful')
-        return True
-    
-    return False
+def reset_position():
+    requests.get(f"{HOST}/position/reset").raise_for_status()
