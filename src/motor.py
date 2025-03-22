0d468793f3f17e6f9b7ea1d8e923dca93a25e05f
Alexis Masson <a.masson555@ntymail.com>
2025-03-22 23:23:00 +0100

    feat: unfied motors interface using websokets

--- src/motor.py
+++ src/motor.py
@@ -1,10 +1,31 @@
-class Motor:
-    """Simulated Motor Controller."""
-    def __init__(self, name):
-        self.name = name
-        self.speed = 0.0
 
-    def set_speed(self, speed):
-        """Set motor speed (float between 0 and 1)."""
-        self.speed = max(0.0, min(1.0, speed))  # Ensure valid range
-        #print(f"{self.name} motor speed: {self.speed:.2f}")
+import struct
+
+import websockets.legacy.client as websockets
+from websockets.exceptions import ConnectionClosedError
+
+ENDPOINT ="ws://haumbot-c5cfb8.local/motors.ws"
+
+class Motors:
+
+    def __init__(self):
+        self.socket = websockets.connect(ENDPOINT)
+        self.vl = 0.0
+        self.vr = 0.0
+
+    async def set_motor_speed(self, vl, vr):
+        self.vl = max(-1, min(vl, 1))
+        self.vr = max(-1, min(vr, 1))
+        motor_buffer = struct.pack('>ff', self.vl, self.vr)
+
+        async with self.socket:
+            await self.socket.send(motor_buffer)
+
+    async def set_speed_left(self, vl):
+        await self.set_motor_speed(vl, self.vr)
+
+    async def set_speed_right(self, vr):
+        await self.set_motor_speed(self.vl, vr)
+
+    async def stop(self):
+        await self.set_motor_speed(0, 0)
